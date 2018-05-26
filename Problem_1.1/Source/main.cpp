b924d3f4d40a9b581468a4c60ccb72b18e1108c0
Lester <lgdelacruz92@gmail.com>
2018-05-26 08:05:29 -0700

    Renamed Solution To Cracking Coding Inteview and Solve Problem 1.5

--- Problem_1.1/Source/main.cpp
+++ Problem_1.1/Source/main.cpp
@@ -5,8 +5,8 @@
 #include <assert.h>
 #include <gtest/gtest.h>
 
-
-void reverse(char* str)
+// Problem 1.2
+void Reverse(char* str)
 {
   if (str != nullptr)
   {
@@ -28,23 +28,128 @@ void reverse(char* str)
   }
 }
 
+// Problem 1.3
+bool IsPermutation(std::string _str1, std::string _str2)
+{
+  bool isPermutation{ false };
+  if (_str1.size() == _str2.size())
+  {
+    std::sort(_str1.begin(), _str1.end());
+    std::sort(_str2.begin(), _str2.end());
+    if (_str1 == _str2)
+      isPermutation = true;
+  }
+  return isPermutation;
+}
+
+// Problem 1.4
+void ReplaceSpaceWithTwenty(char* _str, const unsigned int _strSize)
+{
+  char* end = _str;
+  unsigned int total_str_space = 0;
+  while (*end)
+  {
+    total_str_space++;
+    ++end;
+  }
+  total_str_space--;
+  for (unsigned int real_index = _strSize - 1; real_index > 0; real_index--)
+  {
+    if (_str[real_index] != ' ')
+      _str[total_str_space] = _str[real_index];
+    else
+    {
+      _str[total_str_space] = '0';
+      _str[--total_str_space] = '2';
+      _str[--total_str_space] = '%';
+    }
+    total_str_space--;
+  }
+}
+
+// Problem 1.5
+std::string CompressStr(const std::string& _str)
+{
+  char cur_char = '\0';
+  int cur_char_count = 0;
+  std::vector<char> comp_result;
+
+  const auto flush_comp_chars = [&comp_result, &cur_char, &cur_char_count]()
+  {
+    comp_result.push_back(cur_char);
+    comp_result.push_back('0' + cur_char_count);
+    cur_char_count = 1;
+  };
+
+  for (unsigned int i = 0; i < _str.size(); i++)
+  {
+    if (_str[i] == cur_char || i == 0)
+    {
+      cur_char_count++;
+    }
+    else
+    {
+      if (i != 0)
+      {
+        flush_comp_chars();
+      }
+    }
+    cur_char = _str[i];
+  }
+
+  flush_comp_chars();
+
+  if (comp_result.size() >= _str.size())
+    return _str;
+  else
+  {
+    std::string result(comp_result.begin(), comp_result.end());
+    return result;
+  }
+}
+
 TEST(problem_1_2)
 {
   char* nullStr = nullptr;
-  reverse(nullStr);
+  Reverse(nullStr);
   ASSERT_TRUE(nullStr == nullptr);
 
   char emptyStr[] = "";
-  reverse(emptyStr);
+  Reverse(emptyStr);
   ASSERT_STREQ("", emptyStr);
 
   char str[] = "String";
-  reverse(str);
+  Reverse(str);
   ASSERT_STREQ("gnirtS", str);
 
   char head[] = "head";
-  reverse(head);
+  Reverse(head);
   ASSERT_STREQ("daeh", head);
+}
+
+TEST(problem_1_3)
+{
+  ASSERT_TRUE(IsPermutation("", ""));
+  ASSERT_TRUE(IsPermutation("string", "rginst"));
+}
+
+TEST(problem_1_4)
+{
+  char str[] = "Mr John Smith    ";
+  ReplaceSpaceWithTwenty(str, 13);
+  ASSERT_STREQ("Mr%20John%20Smith", str);
+}
+
+TEST(problem_1_5)
+{
+  const std::string result1 = CompressStr("aabcccccaaa");
+  ASSERT_STREQ("a2b1c5a3", result1.c_str());
+
+  const std::string result2 = CompressStr("abc");
+  ASSERT_STREQ("abc", result2.c_str());
+
+  const std::string result3 = CompressStr("");
+  ASSERT_STREQ("", result3.c_str());
 }
 
 int main(int argc, char **argv)
