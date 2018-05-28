e35f19172df257c52d0644a304196b6e11c90703
Lester <lgdelacruz92@gmail.com>
2018-05-27 19:06:11 -0700

    Problem 2.1: Finished Deleting Duplicated Nodes in a LinkedList

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,50 +1,59 @@
-#include <iostream>
+#include <gtest/gtest.h>
 #include "LinkedList.h"
 #include "Node.h"
 
-int main()
+namespace
 {
-  Node* first;
-  Node* second;
-  Node* third;
+  class LinkedListGoogleTest : public ::testing::Test
   {
-    LinkedList list;
-    list.PushBack(2);
-    list.PushBack(3);
-    list.PushBack(4);
+  protected:
+    void SetUp() override
+    {
+      m_linked_list.PushBack(1);
+      m_linked_list.PushBack(2);
+      m_linked_list.PushBack(3);
+      m_linked_list.PushBack(3);
+      m_linked_list.PushBack(4);
+      m_linked_list.PushBack(3);
+    }
 
-    list.PrintList();
+    void TearDown() override
+    {
 
-    first = list.top_node;
-    second = list.top_node->m_next;
-    third = list.top_node->m_next->m_next;
-    
-    if (first != nullptr)
-      std::cout << "First not null" << std::endl;
-    if (second != nullptr)
-      std::cout << "Second not null" << std::endl;
-    if (third != nullptr)
-      std::cout << "Third is not null" << std::endl;
-    
-    list.Delete(5);
-    list.PrintList();
-    list.Delete(2);
-    list.PrintList();
-    list.Delete(3);
-    list.PrintList();
-    list.Delete(4);
-    list.PrintList();
-    list.Delete(10);
-    list.PrintList();
+    }
+
+    LinkedList m_linked_list;
+  };
+
+  // Problem 1.1
+  void DeleteRepeatedNode(LinkedList* _linked_list)
+  {
+    if (_linked_list->top_node == nullptr)
+      return;
+
+    Node* cur_node = _linked_list->top_node;
+
+    while (cur_node->m_next != nullptr)
+    {
+      if (cur_node->m_next->m_data == cur_node->m_data)
+      {
+        Node* node_to_delete = cur_node->m_next;
+        cur_node->m_next = cur_node->m_next->m_next;
+        delete node_to_delete;
+      }
+      else
+        cur_node = cur_node->m_next;
+    }
   }
 
-  if (first == nullptr)
-    std::cout << "First is null" << std::endl;
-  if (second == nullptr)
-    std::cout << "Second is null" << std::endl;
-  if (third == nullptr)
-    std::cout << "Third is null" << std::endl;
+  TEST_F(LinkedListGoogleTest, problem_1_1)
+  {
 
+  }
+}
+
+int main()
+{
   std::getchar();
   return 0;
 }
